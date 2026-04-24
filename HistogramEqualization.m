dec8990e52cb4393af83c705613c38e712680208
SillyPerson821 <ajcraig3@catamount.wcu.edu>
2026-04-24 15:47:44 -0400

    Update HistogramEqualization.m

--- HistogramEqualization.m
+++ HistogramEqualization.m
@@ -1,13 +1,13 @@
 clear
 clc
-% Step 1: Select file
+% Select the file
 [file, path] = uigetfile({'*.jpg;*png;*.bmp','Select an Image'});
 if isequal(file,0)
 disp('Cancelled');
 return;
 end
 
-% Step 2: Load Image
+% Load the image
 try
 rgb = imread(fullfile(path, file));
 catch err
@@ -15,8 +15,8 @@ errordlg(err.message);
 return;
 end
 
-% Step 3: Convert to grayscale
-img = rgb2gray(rgb);
+img = rgb2gray(rgb); % Set it to grayscale first
+% This next bit is just setting up the other stuff
 newimg = zeros(size(img,1), size(img, 2));
 totalpixels = 0;
 cumuprob = 0;
@@ -24,30 +24,32 @@ tab = zeros(6,256);
 tab(1,:) = 0:1:255;
 for i = 1:size(img,1)
     for j = 1:size(img,2)
-        brightness = img(i,j);
-        tab(2, brightness+1) = tab(2,brightness+1)+1;
-        totalpixels = totalpixels+1;
+        brightness = img(i,j); % This checks the brightness value of each pixel
+        tab(2, brightness+1) = tab(2,brightness+1)+1; % And adds one to a list of how many pixels of the same value there are
+        totalpixels = totalpixels+1; % It also counts how many total pixels there are in the image (for later use)
     end
 end
-for i = 1:size(tab, 2)
-    probability = tab(2,i)/totalpixels;
-    tab(3, i) = probability;
-    cumuprob = probability + cumuprob;
-    tab(4,i) = cumuprob;
-    multicumprob = cumuprob*250;
-    tab(5,i) = multicumprob;
-    fround = floor(multicumprob);
-    tab(6,i) = fround;
+for i = 1:size(tab, 2) % For each of the brightness values:
+    probability = tab(2,i)/totalpixels; % It checks what percentage of pixels have this brightness value
+    tab(3, i) = probability; % And adds the percentage to the table.
+    cumuprob = probability + cumuprob; % It then makes cumulative probability, adding the previous probabilities to the current one.
+    tab(4,i) = cumuprob; % And also adds it to the table.
+    multicumprob = cumuprob*255; % Then, since the probability is from 0-1, it turns it into a brightness value by multiplying it by 255
+    tab(5,i) = multicumprob; % Adds it to the table.
+    fround = floor(multicumprob); % Finally, it rounds that final number down to a whole number.
+    tab(6,i) = fround; % And adds it to the table.
 end
 
+% Then, you replace each pixel in the image with the rounded value from the
+% table
 for i = 1:size(img,1)
     for j = 1:size(img,2)
-        newimg(i,j) = tab(6, img(i,j)+1)/250;
+        newimg(i,j) = tab(6, img(i,j)+1)/255;
     end
 end
 
-% Step 4: Display both
+% Display both images
 figure(1);
 subplot(1,2,1); imshow(img); title(['Original: ' file]);
-subplot(1,2,2); imshow(newimg); title('Changed: ');
+subplot(1,2,2); imshow(newimg); title('Equalized: ');
 whos;
